8fcfa34d36c85bea990a877937dd208c436c45e5
Jordan Ellis <jordanellis620@gmail.com>
2025-07-31 00:34:28 -0400

    Adding game

--- src/data/games.ts
+++ src/data/games.ts
@@ -2,6 +2,148 @@ import { Game, WinType } from "@/types/game";
 import { HeroName } from "@/types/hero";
 import { PlayerName } from "@/types/players";
 
+export function getGameDataByHero() {
+  const record: Record<HeroName, { wins: number; gamesPlayed: number }> = {
+    [HeroName.ARIEN]: {
+      wins: 0,
+      gamesPlayed: 0,
+    },
+    [HeroName.XARGATHA]: {
+      wins: 0,
+      gamesPlayed: 0,
+    },
+    [HeroName.BROGAN]: {
+      wins: 0,
+      gamesPlayed: 0,
+    },
+    [HeroName.WASP]: {
+      wins: 0,
+      gamesPlayed: 0,
+    },
+    [HeroName.SABINA]: {
+      wins: 0,
+      gamesPlayed: 0,
+    },
+    [HeroName.TIGERCLAW]: {
+      wins: 0,
+      gamesPlayed: 0,
+    },
+    [HeroName.DODGER]: {
+      wins: 0,
+      gamesPlayed: 0,
+    },
+    [HeroName.WHISPER]: {
+      wins: 0,
+      gamesPlayed: 0,
+    },
+    [HeroName.MISA]: {
+      wins: 0,
+      gamesPlayed: 0,
+    },
+    [HeroName.URSAFAR]: {
+      wins: 0,
+      gamesPlayed: 0,
+    },
+    [HeroName.SILVERARROW]: {
+      wins: 0,
+      gamesPlayed: 0,
+    },
+    [HeroName.TALI]: {
+      wins: 0,
+      gamesPlayed: 0,
+    },
+    [HeroName.GARRUS]: {
+      wins: 0,
+      gamesPlayed: 0,
+    },
+    [HeroName.BAIN]: {
+      wins: 0,
+      gamesPlayed: 0,
+    },
+    [HeroName.CUTTER]: {
+      wins: 0,
+      gamesPlayed: 0,
+    },
+    [HeroName.TRINKETS]: {
+      wins: 0,
+      gamesPlayed: 0,
+    },
+    [HeroName.NEBKHER]: {
+      wins: 0,
+      gamesPlayed: 0,
+    },
+    [HeroName.BRYNN]: {
+      wins: 0,
+      gamesPlayed: 0,
+    },
+    [HeroName.MORTIMER]: {
+      wins: 0,
+      gamesPlayed: 0,
+    },
+    [HeroName.WIDGET]: {
+      wins: 0,
+      gamesPlayed: 0,
+    },
+    [HeroName.TAKAHIDE]: {
+      wins: 0,
+      gamesPlayed: 0,
+    },
+    [HeroName.EMMITT]: {
+      wins: 0,
+      gamesPlayed: 0,
+    },
+    [HeroName.MIN]: {
+      wins: 0,
+      gamesPlayed: 0,
+    },
+    [HeroName.SWIFT]: {
+      wins: 0,
+      gamesPlayed: 0,
+    },
+    [HeroName.WUK]: {
+      wins: 0,
+      gamesPlayed: 0,
+    },
+    [HeroName.HANU]: {
+      wins: 0,
+      gamesPlayed: 0,
+    },
+    [HeroName.IGNATIA]: {
+      wins: 0,
+      gamesPlayed: 0,
+    },
+    [HeroName.ROWENNA]: {
+      wins: 0,
+      gamesPlayed: 0,
+    },
+    [HeroName.MRAK]: {
+      wins: 0,
+      gamesPlayed: 0,
+    },
+    [HeroName.SNORRI]: {
+      wins: 0,
+      gamesPlayed: 0,
+    },
+    [HeroName.RAZZLE]: {
+      wins: 0,
+      gamesPlayed: 0,
+    },
+    [HeroName.GYDION]: {
+      wins: 0,
+      gamesPlayed: 0,
+    },
+  };
+
+  GAMES.forEach((game) => {
+    game.players.forEach((p) => {
+      record[p.heroName].gamesPlayed++;
+      p.win && record[p.heroName].wins++;
+    });
+  });
+
+	return record;
+}
+
 export const GAMES = [
   {
     id: 1,
@@ -67,5 +209,18 @@ export const GAMES = [
     winType: WinType.LIFE,
     waves: 1,
     type: "short"
+  },
+  {
+    id: 5,
+    date: "07/30/25",
+    players: [
+      {name: PlayerName.NICK, heroName: HeroName.BROGAN, win: true},
+      {name: PlayerName.JORDAN, heroName: HeroName.XARGATHA, win: true},
+      {name: PlayerName.DUSTIN, heroName: HeroName.ARIEN, win: false},
+      {name: PlayerName.CHRIS, heroName: HeroName.TIGERCLAW, win: false}
+    ],
+    winType: WinType.LIFE,
+    waves: 3,
+    type: "long"
   },
 ] as Game[]

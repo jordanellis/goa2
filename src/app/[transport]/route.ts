e6fc3bf4419d4feeb1cdc9498f2d38bcab8d1285
Jordan Ellis <jordanellis620@gmail.com>
2025-09-03 17:10:26 -0400

    Adding more mcp tools

--- src/app/[transport]/route.ts
+++ src/app/[transport]/route.ts
@@ -1,4 +1,6 @@
+import { GAMES } from "@/data/games";
 import { PLAYERS } from "@/data/players";
+import { HeroName } from "@/types/hero";
 import { getPlayerWinrate } from "@/util/match-parser";
 import { createMcpHandler } from "@vercel/mcp-adapter";
 import z from "zod";
@@ -39,21 +41,68 @@ const handler = createMcpHandler(
         };
       }
     );
-    // server.tool(
-    //   "rulesLookup",
-    //   "Lookup a Guards of Atlantis 2 rules question",
-    //   {
-    //     question: z.string(),
-    //   },
-    //   () => ({
-    //     content: [
-    //       {
-    //         type: "text",
-    //         text: "This is my response lol",
-    //       },
-    //     ],
-    //   })
-    // );
+    server.tool(
+      "topPlayerAtAHero",
+      "Find who the best player is with a specific hero",
+      {
+        hero: z.enum(HeroName),
+      },
+      ({ hero }) => {
+        let topRate = -1;
+        let topPlayers: string[] = [];
+
+        PLAYERS.forEach((p) => {
+          let wins = 0;
+          let games = 0;
+          GAMES.forEach((game) => {
+            const player = game.players.find(
+              (matchPlayer: any) =>
+                matchPlayer.name === p.name && matchPlayer.heroName === hero
+            );
+
+            if (!player) return;
+
+            games++;
+            if (player.win) wins++;
+          });
+
+          if (games === 0) return;
+
+          const rate = wins / games;
+
+          if (rate === topRate) topPlayers.push(p.name);
+          if (rate > topRate) {
+            topPlayers = [p.name];
+            topRate = rate;
+          }
+        });
+
+        if (topPlayers.length === 0) {
+          return {
+            content: [
+              {
+                type: "text",
+                text: `No games found for hero ${hero}.`,
+              },
+            ],
+          };
+        }
+        return {
+          content: [
+            {
+              type: "text",
+              text: `With a winrate of ${(topRate * 100).toFixed(
+                2
+              )}%, ${topPlayers.join(" and ")} ${
+                topPlayers.length > 1 ? "are" : "is"
+              } currently the top player${
+                topPlayers.length > 1 ? "s" : ""
+              } with ${hero}`,
+            },
+          ],
+        };
+      }
+    );
   },
   {
     capabilities: {
